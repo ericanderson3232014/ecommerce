4f84865a070b772a85f7a1c7cb710e6378f9b7c4
ericanderson3232014 <ericanderson3262014@gmail.com>
2023-08-26 11:39:09 +0800

    update

--- products/models.py
+++ products/models.py
@@ -111,8 +111,8 @@ class ProductReview(models.Model):
 
 
 class Order(models.Model):
-    customer = models.ForeignKey(User, on_delete=models.PROTECT)
-    product = models.ForeignKey(Product, on_delete=models.PROTECT)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     ordered_date = models.DateTimeField(auto_now_add=True)
     quantity = models.IntegerField(default=1)
     open = models.BooleanField(default=True)
@@ -127,7 +127,7 @@ class Order(models.Model):
     
 
 class Checkout(models.Model):
-    customer = models.ForeignKey(User, on_delete=models.PROTECT)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
     order = models.ManyToManyField(Order)
     checkout_date = models.DateTimeField(auto_now_add=True)
     total_amount_due = models.DecimalField(max_digits=10, decimal_places=0, default=0)
