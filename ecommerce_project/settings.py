d4b9abad271773c0bae4d206872c08b39b0b8a6d
ericanderson3232014 <ericanderson3262014@gmail.com>
2023-09-28 20:50:22 +0800

    update

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.245.37', 'ericanderson3262014.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '192.168.240.37', 'ericanderson3262014.pythonanywhere.com']
 # ALLOWED_HOSTS = []
 
 # Application definition
